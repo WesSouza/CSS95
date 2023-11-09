1427129d9c98c4789464138cad99a07e494c1ab8
Wes Souza <noreply@wes.dev>
2023-11-08 21:59:25 -0500

    Borders all around

--- examples/react-css-modules/src/App.jsx
+++ examples/react-css-modules/src/App.jsx
@@ -2,8 +2,17 @@ import { styles } from "css95";
 
 export function App() {
   return (
-    <div className={styles.ClassicTheme} style={{ "--css95-scale": 2 }}>
+    <div
+      className={styles.ClassicTheme}
+      style={{
+        "--css95-scale": 1,
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+      }}
+    >
       <button className={styles.Button}>Button</button>
+      <input className={styles.Field} />
     </div>
   );
 }
