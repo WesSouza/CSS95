65798b0e27aedc7c6bee01c29e7f56dc41c2d7d8
Wes Souza <noreply@wes.dev>
2023-11-18 19:15:42 -0500

    Spacings and other shenanigans

--- examples/react-css-modules/src/App.jsx
+++ examples/react-css-modules/src/App.jsx
@@ -1,18 +1,46 @@
+import classNames from "classnames";
 import { styles } from "css95";
+import { useEffect } from "react";
 
 export function App() {
+  useEffect(() => {
+    const url = new URL(location.href);
+    const scale = parseFloat(url.searchParams.get("scale") ?? "1");
+
+    document.documentElement.style.setProperty("--css95-scale", scale);
+  }, []);
+
   return (
-    <div
-      className={styles.ClassicTheme}
-      style={{
-        "--css95-scale": 1,
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-      }}
-    >
-      <button className={styles.Button}>Button</button>
-      <input className={styles.Field} />
+    <div className={styles.ClassicTheme}>
+      <div className={classNames(styles.Window, styles.WindowActive)}>
+        <div className={styles.WindowTitleBar}>
+          <div className={styles.WindowTitleIcon}></div>
+          <div className={styles.WindowTitleText}>
+            CSS 95 React CSS Modules Example
+          </div>
+          <div className={styles.WindowTitleButtons}>
+            <button className={styles.WindowTitleButton}></button>
+            <button className={styles.WindowTitleButton}></button>
+            <button className={styles.WindowTitleButton}></button>
+          </div>
+        </div>
+        <div className={classNames(styles.WindowContent, styles.SmallSpacing)}>
+          <div
+            className={classNames(
+              styles.Grouping,
+              styles.Horizontal,
+              styles.SmallSpacing,
+            )}
+          >
+            <button className={styles.Button}>Button</button>
+            <input className={styles.TextBox} />
+          </div>
+          <div className={classNames(styles.Field, styles.SmallSpacing)}>
+            Content
+          </div>
+        </div>
+        <div className={styles.WindowResize}></div>
+      </div>
     </div>
   );
 }
