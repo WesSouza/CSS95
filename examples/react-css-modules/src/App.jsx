d4980c928d94b403684b6282d54a3bc5c913a4b6
Wes Souza <noreply@wes.dev>
2023-12-31 10:57:18 -0500

    Checkboxes and Options

--- examples/react-css-modules/src/App.jsx
+++ examples/react-css-modules/src/App.jsx
@@ -38,6 +38,57 @@ export function App() {
           <div className={classNames(styles.Field, styles.SmallSpacing)}>
             Content
           </div>
+          <fieldset className={styles.GroupBox}>
+            <legend className={styles.GroupBoxLabel}>Option Buttons</legend>
+
+            <div className={styles.Vertical}>
+              <div className={styles.Option}>
+                <input id="option-1" name="option" value="1" type="radio" />
+                <label htmlFor="option-1">Option 1</label>
+              </div>
+              <div className={styles.Option}>
+                <input id="option-2" name="option" value="2" type="radio" />
+                <label htmlFor="option-2">Option 2</label>
+              </div>
+              <div className={styles.Option}>
+                <input id="option-3" name="option" value="3" type="radio" />
+                <label htmlFor="option-3">Option 3</label>
+              </div>
+            </div>
+          </fieldset>
+          <fieldset className={styles.GroupBox}>
+            <legend>Check Boxes</legend>
+
+            <div className={styles.Vertical}>
+              <div className={styles.Checkbox}>
+                <input
+                  id="checkbox-1"
+                  name="checkbox-1"
+                  value="1"
+                  type="checkbox"
+                />
+                <label htmlFor="checkbox-1">Check Box 1</label>
+              </div>
+              <div className={styles.Checkbox}>
+                <input
+                  id="checkbox-2"
+                  name="checkbox-2"
+                  value="2"
+                  type="checkbox"
+                />
+                <label htmlFor="checkbox-2">Check Box 2</label>
+              </div>
+              <div className={styles.Checkbox}>
+                <input
+                  id="checkbox-3"
+                  name="checkbox-3"
+                  value="3"
+                  type="checkbox"
+                />
+                <label htmlFor="checkbox-3">Check Box 3</label>
+              </div>
+            </div>
+          </fieldset>
         </div>
         <div className={styles.WindowResize}></div>
       </div>
